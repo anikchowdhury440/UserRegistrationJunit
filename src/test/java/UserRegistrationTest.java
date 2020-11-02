1717236a74212c4299b3bd0f9f69b077f465ac84
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-02 16:50:00 +0530

    Refactored: Function to Test Password using Rule 4

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -62,14 +62,14 @@ public class UserRegistrationTest {
 	@Test
 	public void testUserPassword_WhenValid_ShouldReturnTrue() {
 		UserRegistration userRegistration = new UserRegistration();
-		boolean isPasswordValid = userRegistration.checkPassword("anikC12345");
+		boolean isPasswordValid = userRegistration.checkPassword("anikCh14");
 		Assert.assertTrue(isPasswordValid);
 	}
 	
 	@Test
 	public void testUserPassword_WhenInvalid_ShouldReturnFalse() {
 		UserRegistration userRegistration = new UserRegistration();
-		boolean isPasswordValid = userRegistration.checkPassword("anik12345");
+		boolean isPasswordValid = userRegistration.checkPassword("anikChowdhury");
 		Assert.assertFalse(isPasswordValid);
 	}
 }
